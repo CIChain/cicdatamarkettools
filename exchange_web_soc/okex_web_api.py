05d5583fcd2fd99d9c82953ce772d84aa47a05eb
mayaoguang <mayaoguang@datebao.com>
2018-05-22 15:49:36 +0800

    web socket keep live

--- exchange_web_soc/okex_web_api.py
+++ exchange_web_soc/okex_web_api.py
@@ -1,41 +1,32 @@
 import websocket
-import sys
-import zlib
-
+import time
+import thread
 
 def on_open(self):
-    self.send("{'event':'addChannel','channel':'ok_sub_futureusd_btc_ticker_this_week'}")
+    self.send("{'event':'addChannel','channel':'ok_sub_spot_eth_btc_deals'}")
+    def run(*args):
+        while True:
+            time.sleep(20)
+            self.send("{'event':'ping'}")
+    thread.start_new_thread(run, ())
 
 def on_message(self,evt):
-    print('------------------')
     print(evt)
-    data = inflate(evt) #data decompress
-    print (data)
     
-def inflate(data):
-    decompress = zlib.decompressobj(
-            -zlib.MAX_WBITS  # see above
-    )
-    inflated = decompress.decompress(data)
-    inflated += decompress.flush()
-    return inflated
-
 def on_error(self,evt):
     print('==================')
     print (evt)
 
 def on_close(self,evt):
     print ('DISCONNECT')
+    
+def on_ping(self):
+    self.send("{'event':'ping'}")
 
 if __name__ == "__main__":
     url = "wss://real.okex.com:10440/websocket/okexapi"      #if okcoin.cn  change url wss://real.okcoin.cn:10440/websocket/okcoinapi
-
     websocket.enableTrace(False)
-    if len(sys.argv) < 2:
-        host = url
-    else:
-        host = sys.argv[1]
-    ws = websocket.WebSocketApp(host,
+    ws = websocket.WebSocketApp(url,
                                 on_message = on_message,
                                 on_error = on_error,
                                 on_close = on_close)
